9c4f10ad942b97cff08e2f35fb6e80c82370f0af
Jesse Trimble <jesseltrimble@gmail.com>
2013-12-02 00:35:53 +0000

    Add to ex11

--- ex11.c
+++ ex11.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+
   int i = 0;
   while (i < argc) {
     printf("arg %d: %s\n", i, argv[i]);
@@ -18,6 +19,12 @@ int main(int argc, char *argv[]) {
     printf("State %d: %s\n", i, states[i]);
     i++;
   }
+
+  printf("\nargv[1] memory position: %u\n", &argv[1]);
+
+  states[0] = argv[1];
+
+  printf("\nstates[0] memory position after states[0] = argv[1]: %u\n", &states[0]);
 
   return 0;
 }
